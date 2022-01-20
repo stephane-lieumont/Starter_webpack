7e1f5ccb1c72cd6c0dd72455b3f028d4a0d0ea23
Stéphane <stephane1920@gmail.com>
2022-01-20 09:28:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Starter Projetc Kit
+![made-with-node](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white) ![made-with-javascript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![made-with-sass](	https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white) ![GitHub repo size](https://img.shields.io/github/repo-size/StephaneLi/StephaneLieumont_6_09122021)  
+
+---
+### Contexte 
+
+Projet de lancement avec les modules necessaires au fonctionnement de webpack 5. 
+
+## Usage 
+Utiliser [node.js](https://nodejs.org/en/download/) pour travailler sur le projet.
+
+Environnement de développement :
+
+```bash
+npm install
+npm run dev
+```
+Environnement de production :
+
+```bash
+npm install
+npm run prod
+```
+
+##  Auteur
+
+Stéphane LIEUMONT  
+[Linkedin](https://www.linkedin.com/in/stephane-lieumont/)  
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+// Chargement des plugins
+const TerserPlugin = require('terser-webpack-plugin') // Minify JS
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // Minify Css
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') // Clean derectories before generate files
+const HtmlWebpackPlugin = require('html-webpack-plugin') // Inject les liens dynamique des fichier hashé
+const ESLintPlugin = require('eslint-webpack-plugin') // ESlint
+
+const path = require('path')
+
+// Verification de l'environnement de developpement
+const dev = process.env.NODE_ENV === 'development'
+
+const config = {
+  mode: process.env.NODE_ENV,
+  watch: dev,
+  devtool: dev ? 'eval-cheap-module-source-map' : 'source-map',
+  entry: ['./src/app.js', './src/scss/app.scss'],
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: dev ? 'js/app.js' : 'js/app_[chunkhash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.json$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'data/[name][ext]'
+        }
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)(\?.*)?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: false
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin({
+      dry: false, // tester la configuration "true" avant de clean
+      cleanStaleWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: [
+        'js/*',
+        'css/*'
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      // cache managment avec hash en prod
+      filename: dev ? 'css/app.css' : 'css/app_[contenthash:8].css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: 'src/index.ejs'
+    }),
+    new ESLintPlugin({
+      overrideConfigFile: path.resolve(__dirname, '.eslintrc')
+    })
+  ]
+}
+
+// Ajout de plugins seulement si mode production
+if (!dev) {
+  config.optimization = {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
+}
+
+module.exports = config
